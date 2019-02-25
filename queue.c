a213e4c1322ba8cebfe3ac6b0f4452d79cab2f0a
czvkcui <czvkcui@gmail.com>
2019-02-26 01:44:02 +0800

    Implement insert head function
    This commit implement insert head for queue.

--- queue.c
+++ queue.c
@@ -60,16 +60,27 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
-    list_ele_t *newh;
-    /* What should you do if the q is NULL? */
-    newh = malloc(sizeof(list_ele_t));
-    /* Don't forget to allocate space for the string and copy it */
-    /* What if either call to malloc returns NULL? */
+    list_ele_t *newh = malloc(sizeof(list_ele_t));
+    int len = strlen(s);
+    if (!q) goto error;
+    if (!newh) goto error;
+    newh->value = malloc((len + 1) * sizeof(char));
+    if (!newh->value) goto error;
+    strncpy(newh->value, s, len);
+    newh->value[len] = '\0';
     newh->next = q->head;
-    q->head = newh;
-    return true;
-}
 
+    if (!q->head)
+    {
+        q->tail = newh;
+    }
+    q->head = newh;
+    q->size++;
+    return true;
+error:
+    if (newh) free(newh);
+    return false;
+}
 
 /*
   Attempt to insert element at tail of queue.
